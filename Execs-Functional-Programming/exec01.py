49d8b4425cd11d7216a20b4774c3b905875461d5
kevin-silva <kevintysilva@gmail.com>
2021-03-10 21:44:24 -0300

    Adicionado inputs nas variáveis para que o programa fique mais dinamico, gerado tambem o type casting das variáveis

--- Execs-Functional-Programming/exec01.py
+++ Execs-Functional-Programming/exec01.py
@@ -6,15 +6,15 @@
 * Obter o imc da pessoa com 2 casa decimais(Peso e altura da pessoa)
 * Exibir um texto com todos os valores na tela usando F-String
 """
-nome = "Kevin"
-idade = 25
-altura = 1.77
-peso = 75.00
-anoAtual = 2021
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: "))
+anoAtual = int(input("Digite o ano atual: "))
 
 anoCalculo = anoAtual - idade
 imc = peso / (altura * 2)
 
-print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso}kg.')
+print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso:.0f}kg.')
 print(f'O IMC de {nome} é {imc:.2f}.')
 print(f'{nome} nasceu em {anoCalculo}.')
