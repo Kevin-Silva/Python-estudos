7d727cd057e04c76a9b3aaa0518fe37262277a62
Kevin Silva <kevintysilva@gmail.com>
2021-05-10 12:46:13 -0300

    Making the reading part of the program

---
+++ roll_dice_game/application/main.py
@@ -0,0 +1,8 @@
+rollDice = input("Do you wanna roll a dice(y/n)?").capitalize()
+
+if rollDice == "Y":
+    print(0)
+elif rollDice == "N":
+    print("Ok! closing the program now!")
+else: 
+    print("Something went wrong! closing program now!")
