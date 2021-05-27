7ef554f3f74f8b8dc8df0958784ec3d3d60bfd1e
Kevin <kevintysilva@gmail.com>
2021-05-27 14:08:49 -0300

    Criado a logica basico do jogo, primeira versão funcionando corretamente! utilizando apenas um arquivo e criando a função de rolar o dado dentro do proprio main. aprimoramentos virão mais a frente!

--- roll_dice_game/application/main.py
+++ roll_dice_game/application/main.py
@@ -1,8 +1,14 @@
+import random
 rollDice = input("Do you wanna roll a dice(y/n)?").capitalize()
 
+def dice():
+    return random.randrange(1, 6)
+
 if rollDice == "Y":
-    print(0)
-elif rollDice == "N":
-    print("Ok! closing the program now!")
-else: 
-    print("Something went wrong! closing program now!")
+    numeroSelecionado = int (input("Choose a number between 1 and 6: "))
+    if(numeroSelecionado == dice()):
+        print("Congratulations you choose the rigth number")
+    else:
+        print("Wrong number!")
+else:
+    print("Closing program.")
